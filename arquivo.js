207df84b983a1daa150c3e597af8466ffc16459b
Kelen Luzia Rohde Vargas <182110019@senacinformatica.br>
2022-09-21 11:50:02 -0300

    enviando atualizações feitas em aula - score

--- arquivo.js
+++ arquivo.js
@@ -1,5 +1,8 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const cloud = document.querySelector(".cloud");
+let score = 0;
+pipePositionScore = pipe.offsetLeft;
 
 const jump = () => {
     mario.classList.add("jump");
@@ -10,23 +13,36 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+
+    const cloudPosition = cloud.offsetLeft;
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
     console.log(pipePosition);
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.style.animation="none";
+        // pra a animação da nuvem parar quando der game over
+        cloud.style.animation = "none";
+        cloud.style.left = `${cloudPosition}px`;
+
+        // pra a animação do cano parar quando der game over
+        pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation="none";
-        mario.style.botton=`${marioPosition}px`;
-        mario.src="assets/img/game-over.png";
-        mario.style.width="75px";
-        mario.style.marginLeft="50px";
+        // quando o mario encostar no cano se dá  game over
+        mario.style.animation = "none";
+        mario.style.botton = `${marioPosition}px`;
+        mario.src = "assets/img/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+        document.getElementById("recomecar").removeAttribute("hidden");
         clearInterval(loop);
-          
+    } else if (pipePosition > pipePositionScore) {
+        score += 100;
+        document.getElementById("score").innerHTML = score;
     }
-}, 10); 
+    pipePositionScore = pipePosition;
+
+}, 10);
 
 document.addEventListener("keydown", jump);
 
